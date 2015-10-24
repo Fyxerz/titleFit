dfcc71801690479ff64cd6d001b871241b9d9755
Pedro Morales Rodriguez <Fyx@MacBook-Pro-de-Pedro.local>
2015-10-24 23:24:48 +0100

    Cleared the code a bit and a debugged, the h3

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
-function init() {
-	heightFit("h2")
-	heightFit("p")
-	lineFit("h3", 3)
-}
+
 
 function heightFit(target) {
-	$(target).each(function() {
-		var titleSize = $(this).outerHeight(true)
-		var parentSize = $(this).parent().height()
+    $(target).each(function() {
+        var titleSize = $(this).outerHeight(true);
+        var parentSize = $(this).parent().height();
 
-		titleFont = $(this).css('font-size').match(/\d+/)
+        var titleFont = $(this).css('font-size').match(/\d+/);
 
 
-		for (titleFont; titleSize > parentSize; titleFont--) {
-			$(this).css('font-size', titleFont)
-			titleSize = $(this).outerHeight(true)
-		}
-	})
+        for (titleFont; titleSize > parentSize; titleFont--) {
+            $(this).css('font-size', titleFont);
+            titleSize = $(this).outerHeight(true);
+        }
+    });
 }
 
 function lineFit(elem, lines) {
+    $(elem).each(function(index, el) {
+        var titleSize = $(this).outerHeight(true);
 
-	$(elem).each(function(index, el) {
-		var titleSize = $(this).outerHeight(true)
+        var titleFont = $(this).css('font-size').match(/\d+/);
 
-		titleFont = $(this).css('font-size').match(/\d+/)
-
-		for (titleFont; titleSize > (titleFont * lines); titleFont--) {
-			$(this).css('font-size', titleFont)
-			titleSize = $(this).outerHeight(true)
-		}
-	})
+        for (titleFont; titleSize > (titleFont * (lines + 1)); titleFont--) {
+            $(this).css('font-size', titleFont);
+            titleSize = $(this).outerHeight(true);
+        }
+    });
 }
 
-init()
+function init() {
+    heightFit('h2');
+    heightFit('p');
+    lineFit('h3', 1);
+    lineFit('.card_title', 1);
+}
+
+init();
